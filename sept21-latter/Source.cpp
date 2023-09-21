6eeb7b571695a087418381532caf0061e33d4901
Work <Work@MSI>
2023-09-21 15:03:02 -0500

    I made some changes! Figure it out!

--- sept21-latter/Source.cpp
+++ sept21-latter/Source.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 
+#include<fstream>
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello World";
+    cout << "NOT hello world" << endl; 
+
+    cout << "Something else!" << endl; 
+
+    ifstream inputFile{ "input.txt" };
+
+    string input; 
+
+    inputFile >> input; 
+
+    cout << input << endl;
 
     return 0;
 }
